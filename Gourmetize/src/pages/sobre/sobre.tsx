59ba7356bf87aec363bace7c6919b66458a46e90
lucasld1224-wq <lucasld1224@gmail.com>
2025-12-13 15:08:17 -0300

    cd

---
+++ Gourmetize/src/pages/sobre/sobre.tsx
@@ -0,0 +1,36 @@
+
+
+export default function Sobre() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto text-slate-900">
+      <h1 className="text-3xl font-bold mb-6">Sobre o Gourmetize</h1>
+
+      <p className="leading-7 text-justify text-lg mb-6" style={{ textIndent: "2em" }}>
+        A StartUp começou como um projeto escolar, no ano de 2025, os alunos do
+        1º ano do Ensino Médio em Tempo Integral, orientados pelos professores
+        Anderson Ioriati Colombelli, Edson Binelo e Ambrósio Kempka,
+        desenvolveram o projeto de uma startup: Gourmetize. O projeto investe em
+        formas sustentáveis de integração entre o pequeno comércio local e os
+        produtores e agricultores familiares, por meio de um aplicativo gratuito,
+        acessível a consumidores e produtores em qualquer dispositivo móvel.
+      </p>
+
+      <p className="leading-7 text-justify text-lg mb-6" style={{ textIndent: "2em" }}>
+       
+        O objetivo principal é permitir que o consumidor tenha acesso direto aos
+        produtos sazonais e artesanais, diretamente de pequenos produtores ou
+        agricultores locais, sem precisar passar por grandes mercados,
+        supermercados ou distribuidores. Isso resultará em alimentos mais
+        frescos, de melhor qualidade e de menor custo.
+      </p>
+
+      <p className="leading-7 text-justify text-lg mb-6" style={{ textIndent: "2em" }}>
+       
+        A StartUp promoveria os produtores familiares, aumentando a
+        sustentabilidade e a preservação do meio ambiente, sem reduzir a
+        qualidade do produto. Sustentando a nossa ideia, temos como base a Agenda
+        2030 e os Objetivos de Desenvolvimento Sustentável (ODS).
+      </p>
+    </div>
+  );
+}
