620e2d584fe271a7e91f372370c3505aec192d3b
david <davidmila60021@gmail.com>
2025-12-20 15:33:23 -0300

    ..

--- Gourmetize/src/components/navbar.tsx
+++ Gourmetize/src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import logo from "../assets/imagens/logo.jpeg";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -6,10 +7,12 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-white dark:bg-slate-900 border-b border-slate-200 dark:border-slate-800">
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-        
-        <h1 className="text-xl font-semibold text-slate-900 dark:text-white">
+        <img src={logo} alt="Logo" className="h-10" />
+        <h1 className="text-xl font-semibold text-slate-900 dark:text-white oswald text-xl font-semibold text-slate-900 dark:text-white">
           Gourmetize
-        </h1>
+       </h1>
+        
+
 
         <button
           onClick={() => setOpen(!open)}
@@ -19,13 +22,13 @@ export default function Navbar() {
         </button>
 
         <ul className="hidden md:flex gap-8 text-slate-700 dark:text-slate-300 text-sm font-medium">
-          <li className="hover:text-amber-500 dark:hover:text-amber-400 cursor-pointer">
+          <li className="hover:text-green-500 dark:hover:text-green-500 cursor-pointer">
             Página inicial
           </li>
-          <li className="hover:text-amber-500 dark:hover:text-amber-400 cursor-pointer">
+          <li className="hover:text-green-500 dark:hover:text-green-500 cursor-pointer">
             Sobre
           </li>
-          <li className="hover:text-amber-500 dark:hover:text-amber-400 cursor-pointer">
+          <li className="hover:text-green-500 dark:hover:text-green-500 cursor-pointer">
             Contatos
           </li>
         </ul>
@@ -45,5 +48,6 @@ export default function Navbar() {
         </ul>
       )}
     </nav>
+    
   );
 }
